a21e8a48e032d07b5bb3479d15a764aaa555087e
Pawan kr <itsmekr.info@gmail.com>
2021-06-20 16:01:57 +0530

    Updates

---
+++ precache-manifest.fc7881472ab6ffddf6137274647dc96f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75d04ef9d49c42fa90e718ae7eec7f34",
+    "url": "/tech-news/index.html"
+  },
+  {
+    "revision": "3983b5a5b16c4192a3cb",
+    "url": "/tech-news/static/css/main.35784568.chunk.css"
+  },
+  {
+    "revision": "2829f658d05344f4543e",
+    "url": "/tech-news/static/js/2.4904b4fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tech-news/static/js/2.4904b4fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3983b5a5b16c4192a3cb",
+    "url": "/tech-news/static/js/main.f032a154.chunk.js"
+  },
+  {
+    "revision": "1c0106df5022f49c441b",
+    "url": "/tech-news/static/js/runtime-main.9541a700.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/github.io/tech-news/precache-manifest.3025b6069b0fb705d15c62f094c77b99.js"
+  "/tech-news/precache-manifest.fc7881472ab6ffddf6137274647dc96f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/github.io/tech-news/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tech-news/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
